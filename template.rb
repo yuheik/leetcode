cf905fc0556e064c79f0dd8942699ba8c4029b63
yuhei.k <yuhei.k@gmail.com>
2019-08-15 12:44:48 +0900

    Divided util codes into another file.

--- template.rb
+++ template.rb
@@ -6,33 +6,6 @@ def main(input)
   return
 end
 
-def dump(array)
-  values = array.map { |val| sprintf("% 2d, ", val) }.join
-  indexs = (0..array.length-1).to_a.map { |val| sprintf("% 2d, ", val) }.join
-
-  puts values
-  puts indexs
-  puts
-end
-
-def binary_search(sorted_array, target)
-  low = 0
-  high = sorted_array.length - 1
-
-  while low <= high
-    mid = (low + (high - low)/2)
-
-    return mid if target == sorted_array[mid]
-    if (target < sorted_array[mid])
-      high = mid - 1
-    else
-      low = mid + 1
-    end
-  end
-
-  return nil
-end
-
 if __FILE__ == $0
   testcases = [
     [[], ""],
